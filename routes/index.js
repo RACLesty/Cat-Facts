e7dcd21022d443f872784676f6d7df7dd70c6725
RACLesty <ztos@msn.com>
2024-02-19 23:37:19 -0500

    Finish Cat Facts

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get("/", function (req, res, next) {
 
 router.get("/cats/fact", async (req, res) => {
   const re = await axios.get("https://catfact.ninja/fact");
-  const fact = res.data.fact;
+  const fact = re.data.fact;
   res.render("cat/fact", { fact });
 });
 
